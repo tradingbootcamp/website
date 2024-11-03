e51bdd37a77cea91a9c1849c9a9d01d023e25c7a
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-11-03 00:43:11 -0400

    tweaks from ross's todo list on discord

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -9,11 +9,11 @@
                 </li>
                 <li class="bg-white p-6 rounded-lg shadow-md">
                     <strong class="text-lg font-semibold">What level of math background is required?</strong>
-                    <p class="text-gray-700">There’s basically no content prerequisites, in that the bootcamp won’t use calculus or linear algebra or anything, but being a person comfortable thinking in mathematical frameworks - in particular the concept of expected value/probability - is pretty important. A lot of trading is about fast heuristic reasoning instead of having precise or sophisticated mathematical models, but those heuristics are still shaped by mathematics intuitions. If you’re “not a math person” it’s likely going to be a bad fit, but if you’re “into math but haven’t taken high level classes yet” that’s 100% fine and common.</p>
+                    <p class="text-gray-700">There’s basically no content prerequisites, in that the bootcamp won’t use calculus or linear algebra or anything. That being said, it's pretty helpful to be a person comfortable thinking in mathematical frameworks - in particular the concept of expected value/probability. A lot of trading is about fast heuristic reasoning instead of having precise or sophisticated mathematical models, but those heuristics are still shaped by mathematics intuitions. If you’re “not a math person” it’s likely going to be a bad fit, but if you’re “into math but haven’t taken high level classes yet” that’s 100% fine and common</p>
                 </li>
                 <li class="bg-white p-6 rounded-lg shadow-md">
                     <strong class="text-lg font-semibold">Is housing provided?</strong>
-                    <p class="text-gray-700">It’s not included in the cost, but you can book rooms at Lighthaven here if desired. People might also try to coordinate housing via the Discord server.</p>
+                    <p class="text-gray-700">It’s not included in the cost, but you can <a target="_blank" href="https://www.havenbookings.space/events/eternal-september" class="text-blue-600 hover:text-blue-800 underline">book rooms at Lighthaven here</a> if desired. People might also try to coordinate housing via the Discord server.</p>
                 </li>
                 <li class="bg-white p-6 rounded-lg shadow-md">
                     <strong class="text-lg font-semibold">What are the demographics of attendees?</strong>
