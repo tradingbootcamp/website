aa4aefc0ff811c7b5b92fa2f56778e471766b3c2
Ricki Heicklen <ricki.heicklen@gmail.com>
2024-11-27 18:23:17 -0800

    adding iconic testimonial by theo

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -6,9 +6,10 @@ export const testimonials = [
   { quote: "The way Ricki teaches the trading bootcamp is extremely engaging and information dense on material you won't find outside of big trading firms. It is a phenomenal experience that you will not find elsewhere.", author: "Niko" },
   { quote: "Ruthlessly efficient learning, with feedback in real-time. Prepare for information density that'll rewire your brain.", author: "Niko" },
   { quote: "I want more Ricki. Blog posts? Podcasts? Videos? I'll take anything.", author: "Niko" },
-  { quote: "If you can figure out a way to make money from this then go for it." },
+  // { quote: "If you can figure out a way to make money from this then go for it." },
   { quote: "Ricki is a fun, effective instructor. Talks at a million miles an hour, takes all questions and new constraints in stride and does something reasonable in response… the Electronic Trading Competition was glorious, relentless chaos.", author: "William" },
   // { quote: "Ricki has a lot of good qualities and none of them are subtlety.", author: "Avital" },
   { quote: "When you're learning all the ways you can be abused in markets, you want to have a teacher as fun, engaging, and smart as Ricki.", author: "Rai" },
-  { quote: "felt too chaotic", author: "Anonymous" }
+  { quote: "felt too chaotic", author: "Anonymous" },
+  { quote: "Ricki is the best teacher I have ever had. I would immediately sign up to anything Ricki is teaching, even if it was teaching Paint Drying Bootcamp", author: "Theo" }
 ]
