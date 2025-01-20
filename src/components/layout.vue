28487b9b0c2055ada7074bfd0db8593d4c685c97
Saul Munn <100555686+saulmunn@users.noreply.github.com>
2025-01-20 11:00:08 -0800

    added "corporate partnerships" redirect to the navbar

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -20,6 +20,7 @@
               <router-link to="/team" class="text-gray-600 hover:text-gray-900">Team</router-link>
               <router-link to="/faq" class="text-gray-600 hover:text-gray-900">FAQ</router-link>
               <router-link to="/socialproof" class="text-gray-600 hover:text-gray-900">Social Proof</router-link>
+              <router-link to="/corporate-partnerships" class="text-gray-600 hover:text-gray-900">Corporate Partnerships</router-link>
               <a target="_blank" :href="registerUrl" class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
                 Register Now
               </a>
@@ -45,6 +46,7 @@
             <router-link to="/team" class="block px-3 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-50">Team</router-link>
             <router-link to="/faq" class="block px-3 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-50">FAQ</router-link>
             <router-link to="/socialproof" class="block px-3 py-2 text-gray-600 hover:text-gray-900 hover:bg-gray-50">Social Proof</router-link>
+            <router-link to="/corporate-partnerships" class="text-gray-600 hover:text-gray-900">Corporate Partnerships</router-link>
             <a :href="registerUrl" class="block w-full bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
               Register Now
             </a>
--- src/data/links.js
+++ src/data/links.js
@@ -6,5 +6,6 @@ export const links = {
   scheduleEmbed: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUpeAUhWp4uMFUeHYo9LHVIYxHdAGj9BYVhITVC5KxlUG2Hi90dwnBaS9MH2aufIXf0rP6stYjbZtr/pubhtml?gid=823240928&amp;single=true&amp;widget=true&amp;headers=false",
   lighthaven: "https://www.lighthaven.space/",
   lighthavenBooking: "https://www.havenbookings.space/events/eternal-september",
-  rickiWebsite: "https://rickiheicklen.com/index.html"
+  rickiWebsite: "https://rickiheicklen.com/index.html",
+  corporatePartnerships: "https://bit.ly/qtb-corporate-partnerships"
 }
