f396bb0feff104e44dbfc180b4635fc2124d582c
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-01-30 22:18:01 -0500

    updating registration to february

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,5 @@
 export const links = {
-  register: "https://bit.ly/qtb-january-2025",
-  registerFeb: "https://bit.ly/qtb-february-2025-reg",
+  register: "https://bit.ly/qtb-february-2025-reg",
   interest: "https://bit.ly/qtb-interest",
   schedule: "https://bit.ly/qtb-schedule",
   scheduleEmbed: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSUpeAUhWp4uMFUeHYo9LHVIYxHdAGj9BYVhITVC5KxlUG2Hi90dwnBaS9MH2aufIXf0rP6stYjbZtr/pubhtml?gid=823240928&amp;single=true&amp;widget=true&amp;headers=false",
